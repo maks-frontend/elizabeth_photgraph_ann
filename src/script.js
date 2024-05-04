c003d564ab2df84784f71d8cb68cbe7fdb720253
maks-frontend <maksimrazinkov@gmail.com>
2024-05-04 16:52:26 +0300

    email-go

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function openModal() {
+            document.getElementById("myModal").style.display = "block";
+        }
+        function closeModal() {
+            document.getElementById("myModal").style.display = "none";
+}
+        
+function openOrder() {
+   document.getElementById("order_form").style.display = "block";
+        }
+        function closeOrder() {
+            document.getElementById("order_form").style.display = "none";
+} 
