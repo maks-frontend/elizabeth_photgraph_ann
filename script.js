dad02b0fe77571de9cca4349f195d3b1825a426d
maks-frontend <maksimrazinkov@gmail.com>
2024-05-04 20:50:34 +0300

    changes

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ function openOrder() {
         function closeOrder() {
             document.getElementById("order_form").style.display = "none";
 } 
+
+
+function openPrice() {
+            document.getElementById("myPrice").style.display = "block";
+        }
+        function closePrice() {
+            document.getElementById("myPrice").style.display = "none";
+        }
+            
+        function openSertificates() {
+            document.getElementById("mySertificates").style.display = "block";
+        }
+        function closeSertificates() {
+            document.getElementById("mySertificates").style.display = "none";
+        }
+            
+
+
+        // core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  ...
+});
