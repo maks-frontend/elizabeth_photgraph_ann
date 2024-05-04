e13bde4da920a669760b9bb4f2db17e3bcb38ff6
maks-frontend <maksimrazinkov@gmail.com>
2024-05-04 23:15:17 +0300

    js

--- script.js
+++ script.js
@@ -27,19 +27,3 @@ function openPrice() {
             document.getElementById("mySertificates").style.display = "none";
         }
             
-
-
-        // core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  ...
-});
---
+++ swiper.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    ...
+}
+);
